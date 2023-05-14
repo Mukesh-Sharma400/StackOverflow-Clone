787070d0427874bd05655920e41c18ed1908608c
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-05-14 13:34:29 +0530

    romove chatbot import

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,9 @@ import thunk from "redux-thunk";
 import Reducers from "./reducers";
 import App from "./App";
 import "./index.css";
-import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 import reportWebVitals from "./reportWebVitals";
 
 const store = configureStore({ reducer: Reducers, middleware: [thunk] });
-Kommunicate.init("13ac92d62fa70403336488603719b00bd");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
